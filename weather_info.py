10742ae827d1d7b331486781b1f4ae2f66e936ee
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-07-04 17:02:09 +0800

    Docs: Add comments

--- weather_info.py
+++ weather_info.py
@@ -6,6 +6,10 @@ import requests
 # MetaWeather provides an API that delivers JSON over HTTPS for access to their data.
 API_ROOT = 'https://www.metaweather.com'
 
-# Location search URL. Argument should be a string.
+# Location search URL. Argument should be a string. This provides a WOEID which
+# we will use later to get the weather.
 API_LOCATION = '/api/location/search/?query='
+
+# After taking the WOEID from the location query, we will use that to get the
+# actual weather for that location.
 API_WEATHER = '/api/location/' # + WOEID
