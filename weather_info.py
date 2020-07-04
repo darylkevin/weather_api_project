c1a524ea0ded005fca3ee0898c814c499c759ef0
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-07-04 20:40:32 +0800

    Code; Add URL completion functions

--- weather_info.py
+++ weather_info.py
@@ -4,7 +4,7 @@ import requests
 
 # Public APIs can are available in  https://github.com/public-apis/public-apis#weather
 # MetaWeather provides an API that delivers JSON over HTTPS for access to their data.
-API_ROOT = 'https://www.metaweather.com'
+API_URL = 'https://www.metaweather.com'
 
 # Location search URL. Argument should be a string. This provides a WOEID which
 # we will use later to get the weather.
@@ -13,3 +13,11 @@ API_LOCATION = '/api/location/search/?query='
 # After taking the WOEID from the location query, we will use that to get the
 # actual weather for that location.
 API_WEATHER = '/api/location/' # + WOEID
+
+def location(input):
+    return requests.get(API_URL + API_LOCATION + input).json()
+
+def weather(woeid):
+    return requests.get(API_URL + API_WEATHER + str(woeid)).json()
+
+
