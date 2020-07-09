b954b3c520239b2eaa227814732e5d5e5dc346f9
Daryl Galvez <daryl.kevin.24@gmail.com>
2020-07-09 15:47:15 +0800

    Feat: Intergrate ambiguity block
    Add block of code to handle ambiguous locations from user input. Block
    of code was from Udacity Python course.

--- weather_info.py
+++ weather_info.py
@@ -14,12 +14,21 @@ API_LOCATION = '/api/location/search/?query='
 # actual weather for that location.
 API_WEATHER = '/api/location/' # + WOEID
 
+# Block where program will send requests to the web API.
 def location(input):
     return requests.get(API_URL + API_LOCATION + input).json()
 
 def weather(woeid):
     return requests.get(API_URL + API_WEATHER + str(woeid)).json()
 
+# Block of code which I intergrated from my Udacity course. This helps eliminate
+# ambiguity in user input.
+def disambiguate_locations(locations):
+    print("Ambiguous location! Did you mean:")
+    for loc in locations:
+        print(f"\t* {loc['title']}")
+
+# This is the terminal output which prints out the weather for the user to see.
 def forecast_printout(weather):
     print(f"The weather for {weather['title']}:")
     for entries in weather['consolidated_weather']:
@@ -30,12 +39,23 @@ def forecast_printout(weather):
         print(f"On {date},it will have {state}, with highs of {high}°C and"
               f"lows of {low}°C.")
 
+# Program asks user for which place they would like to know the weather of.
 def get_forecast():
     try:
-        which_place = input('Which place would you like to know the weather of? \n')
+        # I integrated this block of code from my Udacity Python course which
+        # improves error handling in the weather program. This elimiates ambiguity in
+        # user input.
+        which_place = ''
+        while not which_place:
+            which_place = input('Which place would you like to know the weather of? \n')
         this_place = location(which_place)
-        woeid = this_place[0]['woeid']
-        forecast_printout(weather(woeid))
+        if len (this_place) == 0:
+            print('I don\'t think that\'s a real location.')
+        elif len (this_place) > 1:
+            disambiguate_locations(this_place)
+        else:
+            woeid = this_place[0]['woeid']
+            forecast_printout(weather(woeid))
     except requests.exceptions.ConnectionError:
         print("Please check your connection status. Cannot connect to server.")
 
